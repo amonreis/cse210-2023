b24d13758bcd7483c05ebb870c90d2e048a5b789
amonreis <ammonreis@gmail.com>
2023-09-21 22:14:54 -0300

    finished stretch challenge

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
@@ -8,30 +9,45 @@ class Program
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 100);
 
-        int userGuess = 9999;
+        int attempts = 0;
+        int userGuess = -1;
+        string playAgain = "yes";
 
         // Keep looping until the user guesses the magic number
-        while (userGuess != magicNumber)
+        while (playAgain.ToLower() == "yes")
         {   
             // Ask the user for a guess
             Console.Write("What is your guess? ");
             userGuess = int.Parse(Console.ReadLine());
             
+            // Keep track of how many guesses the user has made
+            attempts += 1;
+
+            // Display a message if they guess the magic number
+            // Display the number of attempts the user made until they guess the number.
+            if (userGuess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you a total of {attempts} attempts.");
+               
+                // Ask if they want to play again
+                Console.Write("Do you want to play again? ");
+                playAgain = Console.ReadLine();
+                // Generate a new random number
+                magicNumber = randomGenerator.Next(1, 100);
+                // Reset the attempts
+                attempts = 0;
+            }
+
             // If the user guess is higher or lower than the magic number 
             // display a message to help them guess it.
-            if (magicNumber < userGuess)
+            else if (magicNumber < userGuess)
             {
                 Console.WriteLine("Lower");
             }
             else if (magicNumber > userGuess)
             {
                 Console.WriteLine("Higher");
-            }
-            
-            // Display a message if they guess the magic number
-            else
-            {
-                Console.WriteLine("You guessed it!");
             }
         }
     }
